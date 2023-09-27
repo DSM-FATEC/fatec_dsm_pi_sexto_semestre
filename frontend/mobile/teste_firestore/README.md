5062a4eca6b40dc5c0f5bf7121dbbd7ef38f71f7
Gustavo Henrique Pinto <ghp2201@gmail.com>
2023-09-26 22:30:34 -0300

    feat: adiciona exemplo de app se comunicando com o firestore

---
+++ frontend/mobile/teste_firestore/README.md
@@ -0,0 +1,19 @@
+# teste_firestore
+
+## Instalação
+
+
+1. Instalar CLI do [Firebase](https://firebase.google.com/?hl=pt-br)
+2. Instalar CLi do [FlutterFire](https://firebase.flutter.dev/)
+3. Autenticar no Firebase com o comando:
+   ```bash
+   firebase login
+   ```
+4. Inicializar o FlutterFire no projeto com o comando:
+   ```bash
+   flutterfire configure
+   ```
+5. Executar projeto com o comando:
+   ```bash
+   flutter run
+   ```
---
+++ frontend/mobile/teste_firestore/lib/main.dart
@@ -0,0 +1,102 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:teste_firestore/firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a blue toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final Stream<QuerySnapshot> _stream =
+      FirebaseFirestore.instance.collection("teste").snapshots();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StreamBuilder<QuerySnapshot>(
+        stream: _stream,
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasError) {
+            return const Text('Something went wrong');
+          }
+
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Text("Loading");
+          }
+
+          return ListView(
+            children: snapshot.data!.docs
+                .map((DocumentSnapshot document) {
+                  Map<String, dynamic> data =
+                      document.data()! as Map<String, dynamic>;
+
+                  return ListTile(
+                    title: Text(data['title']),
+                    subtitle: Text(data['subtitle']),
+                  );
+                })
+                .toList()
+                .cast(),
+          );
+        },
+      ),
+    );
+  }
+}
