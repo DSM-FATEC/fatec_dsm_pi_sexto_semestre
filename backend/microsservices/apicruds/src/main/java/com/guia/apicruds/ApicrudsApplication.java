4a88cf6360e41b29f60412e6f27f0bacbe74c4f4
Gustavo Henrique Pinto <ghp2201@gmail.com>
2023-10-09 21:22:34 -0300

    feat: adiciona base para o microsserviço de cruds

---
+++ backend/microsservices/apicruds/src/main/java/com/guia/apicruds/ApicrudsApplication.java
@@ -0,0 +1,13 @@
+package com.guia.apicruds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApicrudsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApicrudsApplication.class, args);
+	}
+
+}
