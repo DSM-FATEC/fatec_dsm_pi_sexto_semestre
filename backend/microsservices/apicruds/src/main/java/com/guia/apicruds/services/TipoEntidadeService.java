1e9b536717d6db4fcb17571ae53a36c4344d973f
Gustavo Henrique Pinto <ghp2201@gmail.com>
2023-11-05 16:24:17 -0300

    fix: corrige TipoEntidadeService sem @Service

--- backend/microsservices/apicruds/src/main/java/com/guia/apicruds/services/TipoEntidadeService.java
+++ backend/microsservices/apicruds/src/main/java/com/guia/apicruds/services/TipoEntidadeService.java
@@ -10,7 +10,9 @@ import java.util.Optional;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class TipoEntidadeService {
     private TipoEntidadeRepository tipoEntidadeRepository;
 
