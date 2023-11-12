54b7eccbf94bff8c023eb72b33d2e70688f67d4e
Mateus Vinicius da Silva <36384659+MateusVS@users.noreply.github.com>
2023-11-12 19:44:14 +0000

    feat: adicionar endpoint para obter um artefato a partir do nome de sua rede wi-fi

--- backend/microsservices/apicruds/src/main/java/com/guia/apicruds/domain/artefato/repositories/ArtefatoRepository.java
+++ backend/microsservices/apicruds/src/main/java/com/guia/apicruds/domain/artefato/repositories/ArtefatoRepository.java
@@ -7,4 +7,5 @@ import com.guia.apicruds.domain.artefato.entities.Artefato;
 
 @Repository
 public interface ArtefatoRepository extends JpaRepository<Artefato, Integer> {
+    List<Artefato> findByWifi(String wifi);
 }
