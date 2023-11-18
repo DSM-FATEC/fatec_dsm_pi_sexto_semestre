2a854bdc0422d074d34d08c2d47d0c87906d3f29
Gustavo Henrique Pinto <ghp2201@gmail.com>
2023-11-18 00:11:55 -0300

    feat (unfinished): add web template layout

---
+++ frontend/web/src/test/java/com/guiame/web/WebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guiame.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
