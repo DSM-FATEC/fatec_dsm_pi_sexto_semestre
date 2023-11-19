c484cb50362322001a1f469becaa057c3a12b151
Gustavo Henrique Pinto <ghp2201@gmail.com>
2023-11-19 13:48:18 -0300

    deploy: add scripts to build and publish the microsservices docker images to google cloud registry

---
+++ backend/microsservices/build_docker_apicruds.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -x #echo on
+
+VERSAO=1
+IMAGEM="guia-me-apicruds:v$VERSAO"
+REGISTRY="southamerica-east1-docker.pkg.dev/guiame-mobile/imagens/$IMAGEM"
+
+gcloud auth configure-docker southamerica-east1-docker.pkg.dev
+docker build . -f ./deploy/prod/Dockerfile.apicruds -t "$IMAGEM" --platform linux/amd64
+docker tag $IMAGEM $REGISTRY
+docker push $REGISTRY
