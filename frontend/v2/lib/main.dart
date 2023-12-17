7b34ce2375a285f729b122cfb9b0d69466b8d9a0
BZR4 <BZR4@users.noreply.github.com>
2023-12-17 18:22:06 -0300

    v2-front-end
    - Ajustes firestore.

--- frontend/v2/lib/main.dart
+++ frontend/v2/lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:frontend/firebase_options.dart';
 import 'package:frontend/pages/alerts_screen.dart';
 import 'package:frontend/pages/login/sign_in_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
--- frontend/v2/lib/pages/alerts_screen.dart
+++ frontend/v2/lib/pages/alerts_screen.dart
@@ -5,14 +5,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:frontend/services/firestore_service.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  runApp(const AlertsScreenHome());
-}
-
 class AlertsScreenHome extends StatefulWidget {
   const AlertsScreenHome({super.key});
 
